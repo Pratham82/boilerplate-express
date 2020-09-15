0f30ba18ff3fcf6169f3dbf857224b580f9d42fc
Pratham82 <mali.prathamesh82@gmail.com>
2020-09-16 02:28:29 +0530

    Added soln to question 7 -12 and completed Basic node and express section

--- myApp.js
+++ myApp.js
@@ -5,10 +5,25 @@ var path = require("path");
 var absPath = path.join(__dirname, "./views/index.html");
 var dotenv = require("dotenv");
 dotenv.config();
+var bodyParser = require("body-parser");
 
 // --> 7)  Mount the Logger middleware here
+app.use(function middleware(req, res, next) {
+	console.log(`${req.method} ${req.path} - ::${req.ip}`);
+	next();
+});
 
 // --> 11)  Mount the body-parser middleware  here
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+//* testing body parser
+// app.get("/body-parse", (req, res) => {
+// 	res.send({
+// 		res: bodyParser.json(),
+// 	});
+// });
 
 /** 1) Meet the node console. */
 console.log("Hello World");
@@ -42,16 +57,42 @@ app.get("/json", (req, res) =>
 //  place it before all the routes !
 
 /** 8) Chaining middleware. A Time server */
+app.get(
+	"/now",
+	(req, res, next) => {
+		req.time = new Date().toString();
+		res.send({
+			time: req.time,
+		});
+		next();
+	},
+	(req, res) => {}
+);
 
 /** 9)  Get input from client - Route parameters */
-
+app.get("/:word/echo", (req, res) =>
+	res.send({
+		echo: req.params.word,
+	})
+);
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
+app.get("/name", (req, res) =>
+	res.send({
+		name: `${req.query.first} ${req.query.last}`,
+	})
+);
 
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
 
 /** 12) Get data form POST  */
+app.post("/name", (req, res) => {
+	// console.log(req.body.first);
+	res.send({
+		name: `${req.body.first} ${req.body.last}`,
+	});
+});
 
 // This would be part of the basic setup of an Express app
 // but to allow FCC to run tests, the server is already active
